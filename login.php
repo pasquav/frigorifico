8091634a5734c02c5fec5a11746d966570c588c8
pasquav <pasqualottov@gmail.com>
2023-02-12 18:51:03 -0300

    Alterações

--- login.php
+++ login.php
@@ -8,25 +8,19 @@
         <link href="https://fonts.cdnfonts.com/css/gagalin" rel="stylesheet">
     </head>
     <body>
-        <main class="conteudo-centralizado">
-        <form onsubmit="validateUser(this.username.value, this.password.value); return false;">
-
-            <div class="login">
-                <br>
-                <label for="username">Login</label>
-                <br>
-                <input id="username" name="username" type="text" placeholder="Informe seu login">
-                <br>
-                <label for="password">Senha</label>
-                <br>
-                <input name="password" id="password"type="password" placeholder="Informe sua senha">
-                <br>
-                <input type="submit" value="Login">
-                <br>
-            </div>
-
-        </form>   
-        </main>
+    <main class="d-flex justify-content-center align-items-center">
+  <form onsubmit="validateUser(this.username.value, this.password.value); return false;">
+    <div class="form-group">
+      <label for="username">Login</label>
+      <input id="username" name="username" type="text" placeholder="Informe seu login" class="form-control">
+    </div>
+    <div class="form-group">
+      <label for="password">Senha</label>
+      <input name="password" id="password" type="password" placeholder="Informe sua senha" class="form-control">
+    </div>
+    <input type="submit" value="Login" class="btn btn-primary">
+  </form>   
+</main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         <script src="scripts/validaLogin.js" ></script>
     </body>
